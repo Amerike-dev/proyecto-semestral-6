693480edec0825c379b4220b78cfdaed6cd5e6de
TonyZierold <aleos100900@gmail.com>
2025-08-20 16:15:22 -0600

    (feat): clases player y playercontroller
     Las clases player y playercontroller fueron agregadas.

---
+++ Unity/Assets/Scripts/Models/Player.cs
@@ -0,0 +1,14 @@
+
+[Serializable]
+public class Player
+{
+    public int PlayerID { get; private set; }
+    public string PlayerName { get; set; }
+
+    public Player(int playerId, string playerName = "")
+    {
+        //Esto Identifica al jugador
+        PlayerID = playerId;
+        PlayerName = string.IsNullOrEmpty(playerName) ? $"Player {playerId}" : playerName;
+    }
+}
