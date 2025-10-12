19f2cd13196ca650f0da0824bea766341bccc5ba
Leonardo Ramírez Gutiérrez <150188420+LeeonDeChino@users.noreply.github.com>
2025-10-12 16:47:29 -0600

    (feat): Flujo del juego modificado y corregido
    El flujo del juego se modificó a como se solicita en el ticket #183
    
    Se creó una nueva escena (Levels/1-1) que está vacía de momento.
    
    La escena de Level que se tenía anteriormente se conectó al botón de "Test Level" momentáneamente.

---
+++ Unity/Assets/Clases/Scripts/Static/SceneIndex.cs
@@ -0,0 +1,11 @@
+public static class SceneIndex
+{
+    public static int main = 0;
+    public static int menu = 1;
+    public static int level = 2;
+    public static int settings = 3;
+    public static int credits = 4;
+    public static int players = 5;
+    public static int levelSelect = 6;
+    public static int level1 = 7;
+}
